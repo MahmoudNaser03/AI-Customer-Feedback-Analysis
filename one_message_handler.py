b8a84fd676559ed72c7478c391f89ccd1bdcbcde
MahmoudNaser03 <alnasermahmoud0@gmail.com>
2025-07-17 11:28:11 +0300

    Update one_message_handler.py

--- one_message_handler.py
+++ one_message_handler.py
@@ -2,17 +2,32 @@ import requests
 import streamlit as st
 import json
 
-
 st.title("Customer Message Classifier")
 message=st.text_input("Enter your message :")
 
-response = requests.post(
-    "http://127.0.0.1:5000/classify",
-    json={"message": message}
-)
-data = response.json()
+
+client = OpenAI(api_key = "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")
+
+def classify_message(message):
+    prompt=f"""Classify the following customer message. provide:
+1. Sentiment: Positive, Neutral, or Negative
+2. Intent(s): List **all applicable** intents from [Request Info, Complaint, Cancel Account, Feedback, Refund, ...]. 
+
+Message:
+{message}
+
+Respond in a list of two text elements:['Sentiment :...', 'Intent : ..., ...']
+
+"""
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}]
+    )
+    result = ast.literal_eval(completion.choices[0].message.content)
+
+   
 
 if st.button("Classify"):
-    st.write(data["sentiment"])
-    st.write(data["intent"])
+    st.write(result)
+   # st.write(data["intent"])
 
