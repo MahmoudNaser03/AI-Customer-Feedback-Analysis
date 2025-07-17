3455dd09a0c1d4c87b42785b1d39f08d15097ea0
MahmoudNaser03 <alnasermahmoud0@gmail.com>
2025-07-17 11:31:47 +0300

    Update one_message_handler.py

--- one_message_handler.py
+++ one_message_handler.py
@@ -1,15 +1,18 @@
 import requests
 import streamlit as st
 import json
+from openai import OpenAI
+import ast
 
 st.title("Customer Message Classifier")
-message=st.text_input("Enter your message :")
+message = st.text_input("Enter your message :")
 
+client = OpenAI(
+    api_key="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")
 
-client = OpenAI(api_key = "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")
 
 def classify_message(message):
-    prompt=f"""Classify the following customer message. provide:
+    prompt = f"""Classify the following customer message. provide:
 1. Sentiment: Positive, Neutral, or Negative
 2. Intent(s): List **all applicable** intents from [Request Info, Complaint, Cancel Account, Feedback, Refund, ...]. 
 
@@ -20,14 +23,13 @@ Respond in a list of two text elements:['Sentiment :...', 'Intent : ..., ...']
 
 """
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": prompt}]
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
     )
     result = ast.literal_eval(completion.choices[0].message.content)
+    return  result
 
-   
 
 if st.button("Classify"):
-    st.write(result)
-   # st.write(data["intent"])
-
+    st.write(classify_message(message))
+# st.write(data["intent"])
